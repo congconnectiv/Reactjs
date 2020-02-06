22ed40b728c96fde2244333b0c115014334dae97
du cong <cong.du.connectiv@gmail.com>
2020-02-06 14:09:55 +0700

    [add] splice product for commit previously

--- documents/base-syntax-assignment-problem/base-syntax--assignment-problem/src/components/working-list-conditionals/condition.js
+++ documents/base-syntax-assignment-problem/base-syntax--assignment-problem/src/components/working-list-conditionals/condition.js
@@ -24,9 +24,27 @@ class Condition extends Component {
         this.setState({isToggle: !toggle});
     }
 
+    onDeleteProduct = (productIndex) => {
+        const product = this.state.products;
+        product.splice(productIndex, 1);
+        this.setState({products: product});
+    }
+
     render() {
+
+        const style = {
+            border: '1px solid #ccc',
+            backgroundColor:'#e8e8e8',
+            margin: '2em',
+            padding: '1em'
+        }
+        
         let productItem = this.state.products.map((product, index) => {
-            return product.status ? <Product key={index} product={product} /> : null
+            const content =   <div className="wrapp-product" style={style} key={index}>
+                            <button onClick={this.onDeleteProduct.bind(this,index)} >Delete Product</button>
+                            <Product product={product} />
+                        </div>
+            return (product.status===true) ? content : null;
         });
 
         return (
@@ -34,8 +52,7 @@ class Condition extends Component {
                 <h2>Just click button to diplay content!</h2>
                 <button onClick={this.toggleProductHandle}>Toggle Products</button>
                 {
-                    this.state.isToggle === true ?
-                    productItem : null
+                    this.state.isToggle === true ? productItem : null
                 }
             </div>
         );
